82c383ebad139b2ca430fb2e8cb5556399b192e1
Thierry Lagasse <epagneul@dogstudio.be>
2017-08-17 14:58:10 +0200

    Typo

--- contents/main.php
+++ contents/main.php
@@ -10,7 +10,7 @@
 return [
     'title' => "Inès Pirard",
     'tagline' => "I wanna be a Scottish",
-    'description' => "Dompter des licornes et des bisounours, c’est fait. Maintenant...Je veux un nouveau défi!",
+    'description' => "Dompter des licornes et des bisounours, c’est fait. Maintenant... J’aimerais un nouveau défi!",
 
     // HEAD
     'favicon' => false,
